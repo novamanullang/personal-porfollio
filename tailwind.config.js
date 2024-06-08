918030d8fa44db58d127b28d0ba242dd668bf62c
novamanullang <novamanullang@gmail.com>
2024-06-08 13:02:19 +0700

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'background': '#29241e',
+        'text': '#f7f9fa',
+        'title': '#4cb6f7',
+        'title-hover': '#5ca8ff',
+        'bg': '#1d2120',
+      }
+    },
+  },
+  plugins: [],
+}
+
